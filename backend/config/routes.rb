df0904c337f3aec6932465252de9a0f592926341
Okkun555 <y.okuno.btm@gmail.com>
2025-01-06 09:02:09 +0900

    fetch current user info

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   scope :api do
     mount_devise_token_auth_for 'User', at: 'auth'
 
+    namespace :current do
+      resource :user, only: [:show]
+    end
+
     get "up" => "rails/health#show", as: :rails_health_check
   end
 end
