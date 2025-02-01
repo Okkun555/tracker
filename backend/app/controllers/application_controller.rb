6e56c90119f0981cd370be64f53218bd65c9a075
Okkun555 <y.okuno.btm@gmail.com>
2025-02-01 21:04:03 +0900

    [refactor]profile@showのリファクタ

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::API
   include DeviseHackFakeSession
 
   protected
+  
+  def render_not_found
+    render json: { 'message' => 'リソースが見つかりません。' }, status: :not_found
+  end
 
   def render_bad_request(message)
     render json: { 'message' => message }, status: :bad_request
--- backend/app/controllers/user/profiles_controller.rb
+++ backend/app/controllers/user/profiles_controller.rb
@@ -2,22 +2,11 @@ class User::ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @current_user = current_user
-    profile = @current_user.profile
-
-    if profile
-      render json: {
-        id: profile.id,
-        name: profile.name,
-        introduction: profile.introduction,
-        birthday: profile.birthday,
-        gender: profile.gender
-      }, status: :ok
-    else
-      render json: {
-        message: 'プロフィールが見つかりませんでした。'
-      }, status: :not_found
-    end
+    @profile = current_user.profile
+    
+    return render_not_found unless @profile
+    
+    render json: @profile.as_json
   end
 
   def create
